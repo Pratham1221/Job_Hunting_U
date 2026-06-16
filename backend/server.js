559ec0952b3c76529e3c41feda62675def73411f
pratham1221 <pratham.bansal21@gmail.com>
2026-06-15 17:43:28 -0700

    add db port env

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-// require('dotenv').config();
-// const path = require('path');
-// require('dotenv').config({
-//   path: path.resolve(__dirname, '../.env')
-// });
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
